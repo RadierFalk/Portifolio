49fb6ea21bcac33ab6d414f924fedd32b08cd79d
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-06-01 21:53:39 -0400

    Create container contact information

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import SocialNetworks from './SocialNetworks'
 import Avatar from '../img/Eu3.jpeg'
 
 import '../styles/components/sidebar.sass'
+import InformationContainer from './InformationContainer'
 
 const Sidebar = () => {
   return (
@@ -10,7 +11,7 @@ const Sidebar = () => {
       <img src={Avatar} alt="Radier Falcao" />
       <p className="title">Desenvolvedor Front-End</p>
       <SocialNetworks />
-      <p>Contatos</p>
+      <InformationContainer/>
       <a href="" className="btn">Download Curriculo</a>
     </aside>
   )
