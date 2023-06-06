4e61cc3ddf9bf57c9ddb33d011686a552a1fc536
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2023-06-06 00:48:53 -0400

    Update SocialNetworks.jsx

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -12,7 +12,7 @@ const SocialNetworks = () => {
   return (
   <section id='social-networks'>
     {socialNetworks.map((network) => (
-        <a href="#" className='social-btn' id={network.name} key={network.name}>
+        <a href="https://github.com/RadierFalk"  className='social-btn' id={network.name} key={network.name}>
             {network.icon}
         </a>
     ))}
